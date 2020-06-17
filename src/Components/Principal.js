02e5de6da366370daf08724523954931e7e3acb4
kevin <kevin.martinez@gnp.com.mx>
2020-06-17 00:49:11 -0500

    @Update create fix button icon

--- src/Components/Principal.js
+++ src/Components/Principal.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import { forwardRef } from 'react';
 import { Container, Table,AppBar, IconButton } from '@material-ui/core';
 import MaterialTable from 'material-table';
 import MenuIcon from '@material-ui/icons/Menu';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { AddBox, ArrowDownward } from "@material-ui/icons";
+import { AddBox, ArrowDownward, FirstPage, LastPage, Search, ChevronLeft, ChevronRight, Clear, Check, Delete } from "@material-ui/icons";
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -15,7 +12,17 @@ import axios from 'axios';
 const config = {
     headers: {'Access-Control-Allow-Origin': '*'}
 };
-
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  };
 export class Principal extends React.Component {
     constructor(props) {
       super(props);
@@ -27,7 +34,8 @@ export class Principal extends React.Component {
             { title: 'NOMBRE', field: 'nombre' },
             { title: 'PRIMER APELLIDO', field: 'primer_apellido'},
             { title: 'SEGUNDO APELLIDO', field: 'segundo_apellido'},
-            { title: 'EMAIL', field: 'correo_electronico'  }
+            { title: 'EMAIL', field: 'correo_electronico'  },
+            { title: 'Acciones', field: 'action'  }
           ]
          };
     }
@@ -40,6 +48,24 @@ export class Principal extends React.Component {
             this.setState({ rows });
         })
         
+    }
+
+    index() {
+        axios.get(`http://localhost:3333/api/v1/usuarios`)
+        .then(res => {
+            const rows = res.data.data;
+            console.log(rows);
+            this.setState({ rows });
+        })
+    }
+
+    create (data) {
+        axios.post(`http://localhost:3333/api/v1/usuarios`, data )
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+          return res.data;
+        });
     }
 
  
@@ -65,18 +91,17 @@ export class Principal extends React.Component {
                 <MaterialTable
                     title="Administración de usuarios"
                     columns={this.state.columns}
+                    icons={tableIcons}
                     data={this.state.rows}
                     editable={{
                         onRowAdd: (newData) =>
                         new Promise((resolve) => {
-                            setTimeout(() => {
+                            this.create(newData);
                             resolve();
-                            this.setState((prevState) => {
-                                const data = [...prevState.data];
-                                data.push(newData);
-                                return { ...prevState, data };
-                            });
-                            }, 600);
+                            setTimeout(()=>{
+                                
+                                this.index();
+                            })
                         })
                     }}
                     />
